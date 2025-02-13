61f8921daa8ec10efa747986c6e9c7f7e764b087
azismmz <mochamad.mmz@bsi.ac.id>
2025-02-13 11:36:12 +0700

    Update Spen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             // CalcMemberExpenseJob::dispatch()->onConnection('sync');
             CalcMemberExpenseJob::dispatch(0);
-        })->dailyAt('10:50')->timezone('Asia/Jakarta');
+        })->dailyAt('11:40')->timezone('Asia/Jakarta');
 
         // $schedule->call(function () {
         //     // CalcMemberExpenseJob::dispatch()->onConnection('sync');
